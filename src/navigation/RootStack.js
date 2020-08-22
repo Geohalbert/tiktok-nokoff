2d64b27655f35b123555b590bf92e992c316709b
Hal Peters <geohalbert@gmail.com>
2020-08-22 14:02:30 -0500

    removed headers

--- src/navigation/RootStack.js
+++ src/navigation/RootStack.js
@@ -8,8 +8,16 @@ const Stack = createStackNavigator();
 export default function RootStack() {
   return (
     <Stack.Navigator mode="modal">
-      <Stack.Screen name="Main" component={HomeTabNavigator} />
-      <Stack.Screen name="Live" component={CameraScreen} />
+      <Stack.Screen
+        name="Main"
+        component={HomeTabNavigator}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Live"
+        component={CameraScreen}
+        options={{ headerShown: false }}
+      />
     </Stack.Navigator>
   );
 }
