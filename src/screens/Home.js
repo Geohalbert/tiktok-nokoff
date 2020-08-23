c3d2e581a0d17ae3f7491b6890d4a1e8788e8e20
Hal Peters <geohalbert@gmail.com>
2020-08-22 22:32:05 -0500

    video feed is displaying, swipable

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,11 +1,40 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
+
+import ViewPager from "@react-native-community/viewpager";
+
+import FeedHeader from "../components/FeedHeader";
+import FeedView from "../components/FeedView";
+
+import db from "../../db.json";
+import { headerOptions } from "../constants";
 import { screen } from "../styles/screen";
 
 export default function Home() {
+  const [tab, setTab] = useState(1);
+  const [active, setActive] = useState(0);
+
   return (
     <View style={screen.container}>
-      <Text style={screen.text}>Home</Text>
+      <FeedHeader
+        options={headerOptions}
+        selectedValue={tab}
+        onSelect={setTab}
+      />
+      <ViewPager
+        onPageSelected={e => {
+          setActive(e.nativeEvent.position);
+        }}
+        orientation="vertical"
+        style={{ flex: 1 }}
+        initialPage={0}
+      >
+        {db.videos.map(item => (
+          <View key={item.id}>
+            <FeedView video={item} play={item.id === active} />
+          </View>
+        ))}
+      </ViewPager>
     </View>
   );
 }
