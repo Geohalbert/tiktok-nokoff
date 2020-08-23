ea27c9cd4955ab069b431e9f52681ee549e43a5f
Hal Peters <geohalbert@gmail.com>
2020-08-22 22:28:55 -0500

    added component for videos in feed

---
+++ src/components/FeedView.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Dimensions, StyleSheet, View } from "react-native";
+
+import { Video } from "expo-av";
+
+const width = Dimensions.get("window").width;
+const height = Dimensions.get("window").height;
+const FeedView = props => {
+  const { play, video } = props;
+
+  return (
+    <View style={styles.container}>
+      <Video
+        source={{ uri: video.uri }}
+        isMuted={false}
+        resizeMode="cover"
+        shouldPlay={play}
+        style={{
+          width: "100%",
+          height: "100%"
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#fff",
+    position: "absolute",
+    height: height,
+    width: width,
+    zIndex: -1
+  }
+});
+
+export default FeedView;
