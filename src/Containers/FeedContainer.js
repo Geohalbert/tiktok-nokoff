ec89f8bb07afddf6501d6161343c7df306482a7b
Hal Peters <geohalbert@gmail.com>
2020-08-25 15:04:49 -0500

    reorganizing structure

---
+++ src/Containers/FeedContainer.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { Dimensions, StyleSheet, View } from "react-native";
+
+import ViewPager from "@react-native-community/viewpager";
+import FeedScreen from "../components/FeedScreen";
+
+import db from "../../db.json";
+const { height, width } = Dimensions.get("window");
+const FeedContainer = () => {
+  const [active, setActive] = useState(0);
+  return (
+    <View style={styles.container}>
+      <ViewPager
+        onPageSelected={e => {
+          setActive(e.nativeEvent.position);
+        }}
+        orientation="vertical"
+        style={{ flex: 1 }}
+        initialPage={0}
+      >
+        {db.videos.map(item => (
+          <View key={item.id}>
+            <FeedScreen video={item} play={Number(item.id) === active} />
+          </View>
+        ))}
+      </ViewPager>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "transparent",
+    position: "absolute",
+    flex: 1,
+    height: height,
+    width: width,
+    zIndex: 0
+  }
+});
+
+export default FeedContainer;
